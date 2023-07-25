017b3c02a8a33dad363468c501d0272c2a4ae341
Vũ Hà Minh Trang <78261243+beckachuu@users.noreply.github.com>
2023-07-25 23:57:20 +0700

    add database version and migration

--- app/src/main/java/com/beckachu/androidfeed/data/AppDatabase.java
+++ app/src/main/java/com/beckachu/androidfeed/data/AppDatabase.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.beckachu.androidfeed.data.entities.MyAppEntity;
 import com.beckachu.androidfeed.data.entities.NotiEntity;
@@ -17,7 +19,7 @@ import com.beckachu.androidfeed.data.local.dao.NotiDao;
                 MyAppEntity.class
         },
         exportSchema = false,
-        version = 1
+        version = 2
 )
 public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase db = null;
@@ -28,7 +30,7 @@ public abstract class AppDatabase extends RoomDatabase {
     public abstract MyAppDao myAppDao();
 
     /**
-     * @param context: you should use app context (getApplicationContext()) to avoid
+     * @param context: You should use app context (getApplicationContext()) to avoid
      *                 the activity or service be killed by the system (which leads to
      *                 unexpected issues).
      */
@@ -36,8 +38,29 @@ public abstract class AppDatabase extends RoomDatabase {
         if (db == null) {
             db = Room.databaseBuilder(context.getApplicationContext(),
                             AppDatabase.class, DATABASE_NAME)
+                    .addMigrations(MIGRATION_1_2)
                     .build();
         }
         return db;
     }
+
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            // Create the new table
+//            database.execSQL("DROP TABLE IF EXISTS myappentity");
+            database.execSQL("CREATE TABLE IF NOT EXISTS myappentity " +
+                    "(packageName TEXT PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                    "appName TEXT, isFavorite BOOLEAN, isReceivingNoti BOOLEAN)");
+
+            // Copy the data
+//            database.execSQL("INSERT INTO users_new (userid, username, last_update) SELECT userid, username, last_update FROM users");
+
+            // Remove the old table
+//            database.execSQL("DROP TABLE users");
+
+            // Change the table name to the correct one
+            // database.execSQL("ALTER TABLE users_new RENAME TO users");
+        }
+    };
 }
