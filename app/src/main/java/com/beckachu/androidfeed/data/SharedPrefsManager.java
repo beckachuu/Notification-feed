b5d5505f06962ab6ca74d4dddd8e2ec0f0fb3b4a
Vũ Hà Minh Trang <78261243+beckachuu@users.noreply.github.com>
2023-07-27 22:51:52 +0700

    add SharedPrefsManager

---
+++ app/src/main/java/com/beckachu/androidfeed/data/SharedPrefsManager.java
@@ -0,0 +1,51 @@
+package com.beckachu.androidfeed.data;
+
+import android.content.SharedPreferences;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class SharedPrefsManager {
+    public static final String DEFAULT_NAME = "General";
+    public static final String UNREAD_COUNT = "UNREAD_COUNT";
+
+
+    public static String getString(SharedPreferences prefs, String key, String defaultValue) {
+        return prefs.getString(key, defaultValue);
+    }
+
+    public static void putString(SharedPreferences prefs, String key, String value) {
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static int getInt(SharedPreferences prefs, String key, int defaultValue) {
+        return prefs.getInt(key, defaultValue);
+    }
+
+    public static void putInt(SharedPreferences prefs, String key, int value) {
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(key, value);
+        editor.apply();
+    }
+
+    public static List<String> getStringList(SharedPreferences prefs, String key) {
+        Set<String> set = prefs.getStringSet(key, null);
+        if (set != null) {
+            return new ArrayList<>(set);
+        }
+        return null;
+    }
+
+    public static void putStringList(SharedPreferences prefs, String key, List<String> list) {
+        Set<String> set = new HashSet<>(list);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putStringSet(key, set);
+        editor.apply();
+    }
+
+
+}
